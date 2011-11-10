498f6aa0332f9ba274d7bbd361c77dc8b5dd2721
Alexander Yakushev <yakushev.alex@gmail.com>
2011-11-11 01:14:25 +0200

    Update version numbers and add modify README

--- awesompd.lua
+++ awesompd.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------------
 -- @author Alexander Yakushev <yakushev.alex@gmail.com>
 -- @copyright 2010-2011 Alexander Yakushev
--- @release v1.1.0pre
+-- @release v1.1.0
 ---------------------------------------------------------------------------
 
 awesompd = {}
--- jamendo.lua
+++ jamendo.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------------
 -- @author Alexander Yakushev <yakushev.alex@gmail.com>
 -- @copyright 2011 Alexander Yakushev
--- @release v1.1.0pre
+-- @release v1.1.0
 ---------------------------------------------------------------------------
 
 -- Grab environment
